0c9c7cc9fb118a2b65b623ffba22d8a4f10dec1c
YUL-07 <YUL-07@users.noreply.github.com>
2020-11-16 14:50:35 +0900

    first-1

---
+++ script/script.js
@@ -0,0 +1,28 @@
+$(function() {
+    $("#memu").click(function() {
+        $("#modal").css({
+            "display": "block"
+        });
+    })
+    $("#cls").click(function() {
+        $("#modal").css({
+            "display": "none"
+        });
+    })
+});
+
+$(function() {
+    $("div#memu").mouseover(function() {
+        $("i#memuOff").css({ "display": "none" }),
+            $("i#memuOn").css({
+                "display": "inline-block",
+                "font-size": "50px"
+            })
+    });
+    $("div#memu").mouseout(function() {
+        $("i#memuOff").css({ "display": "inline-block" }),
+            $("i#memuOn").css({
+                "display": "none"
+            })
+    });
+});
